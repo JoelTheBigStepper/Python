dfba31799dbc9993b7408f1a1ee26e341322f89d
JoelTheBigStepper <caesarthebigstepper@gmail.com>
2025-10-16 12:45:30 +0100

    more on boolean 2

---
+++ convertion.py
@@ -0,0 +1,19 @@
+# Converting string to integer
+age_str = "25"
+age_int = int(age_str)
+print(f"age:{age_int}")\
+
+# Converting string to float
+height_str = "1.75"
+height_float = float(height_str)
+print(f"height:{height_float}")
+
+# Converting to boolean
+print(bool(0))          # False
+print(bool(42))         # True
+print(bool(""))         # False
+print(bool("Hello"))    # True
+
+# converting boolean to integer
+print(int(True))        # 1
+print(int(False))       # 0
