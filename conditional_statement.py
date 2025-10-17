b5915600cb5bcf844aa14035b94e12a97ec1a091
JoelTheBigStepper <caesarthebigstepper@gmail.com>
2025-10-17 19:01:21 +0100

    started with conditional statements

---
+++ conditional_statement.py
@@ -0,0 +1,18 @@
+age = 20
+if age < 18:
+    print("You are a minor.")
+else:
+    print("You are an adult.")
+
+# more complex conditional statement elif
+score = 85
+if score >= 70:
+    print('you got an A')
+elif score >= 60:
+    print('you got a B')
+elif score >= 50:
+    print('you got a C')
+elif score >= 40:
+    print('you got a D')
+else:
+    print('you got an F')
