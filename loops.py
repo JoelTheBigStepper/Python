30fd5ad357e331bf4924e6f18a5a59932aaa01df
JoelTheBigStepper <caesarthebigstepper@gmail.com>
2025-10-18 20:09:55 +0100

    wl

--- loops.py
+++ loops.py
@@ -13,14 +13,30 @@
 #     print(num)
 
 
-import random
-height = random.randint(1, 1000)
-guess = 0
-while height != guess:
-    guess = int(input('guess the height: '))
-    if guess < height:
-        print('number too low')
-    elif guess > height:
-        print('guess too high')
+# import random
+# height = random.randint(1, 1000)
+# guess = 0
+# while height != guess:
+#     guess = int(input('guess the height: '))
+#     if guess < height:
+#         print('number too low')
+#     elif guess > height:
+#         print('guess too high')
+#     else:
+#         print('congratulation you guessed it')
+
+
+
+# Nested Loops and Loop Control Statements
+for num in range(2, 10):
+    for i in range(2, num): 
+        if num % i == 0:
+            print(f"{num} is not prime")
+            break
     else:
-        print('congratulation you guessed it')
+        print(f"{num} is prime")
+
+
+for i in range(3):
+    for j in range(2):
+        print(f"i: {i}, j: {j}")
