a3f47eff1a9d25a6e753a065bd78a688f2265f6f
JoelTheBigStepper <caesarthebigstepper@gmail.com>
2025-10-21 19:36:58 +0100

    doing more pratiices on loops

--- loops.py
+++ loops.py
@@ -1,10 +1,29 @@
-# for i in range(5):
-#     print(i)
+for i in range(5):
+    print(i)
+    if i == 3:
+        break
 
+
+
+
+for i in range(10):
+    print('hello world', i)
+
+
+
+
+height = 1.745
+guess = 0
+while height != guess:
+    guess = float(input('input height:'))
+    if guess > height:
+        print('greater than height')
+    elif guess < height:
+        print('lesser than height')
+    else:
+        print('correct height')
 
 # count = 0
-# while count <= 5:
-#     print(count)
 #     count += 1
 
 
@@ -28,15 +47,15 @@
 
 
 # Nested Loops and Loop Control Statements
-for num in range(2, 10):
-    for i in range(2, num): 
-        if num % i == 0:
-            print(f"{num} is not prime")
-            break
-    else:
-        print(f"{num} is prime")
+# for num in range(2, 10):
+#     for i in range(2, num): 
+#         if num % i == 0:
+#             print(f"{num} is not prime")
+#             break
+#     else:
+#         print(f"{num} is prime")
 
 
-for i in range(3):
-    for j in range(2):
-        print(f"i: {i}, j: {j}")
+# for i in range(3):
+#     for j in range(2):
+#         print(f"i: {i}, j: {j}")
--- simple_loin_system.py
+++ simple_loin_system.py
@@ -1,19 +1,3 @@
-# while True:
-#     password = input('input login password (or exit to stop):')
-#     if password.lower() == 'exit':
-#         break
-
-#     if len(password) < 9:
-#         print('password is too short')
-#     elif password == 'python123':
-#         print('access granted')
-#         break
-#     else:
-#         print('wrong password try again')
-
-
-
-
 while True:
     password = input('Input login password (or exit to stop): ').strip()
     if password.lower() == 'exit':
